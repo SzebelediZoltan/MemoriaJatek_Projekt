c4dc88f09a8cb0ead13bad0ad7615cc16f4e953e
Szebelédi Zoltán <szebeledi.zoltan@ckik.loc>
2025-04-15 09:01:10 +0200

    Sok minden készen lett pl start meg ilyenek validating ja

--- Program/script.js
+++ Program/script.js
@@ -1,12 +1,92 @@
 let difficulty;
+const CARDS = ["dabbaby", "datboy", "lean", "oioioi", "orange", "shrek", "skibidi"]
+
+const startButton = document.querySelector("#startButton")
+startButton.addEventListener("click", handleStart)
+
+function handleStart() {
+    let problems = validData()
+    if(!problems.includes(false)) {
+        let actCards = generateCards()
+        resetLogIn()
+        startGame(actCards)
+    } else {
+        showNotValid(problems)
+    }
+}
+
+function resetLogIn() {
+    
+}
+
+function showNotValid(problems) {
+    let error
+    
+    if (problems[2] == false) {
+        error = "Select a difficulty"
+    } else if (problems[0] == false) {
+        error = "Give us a real email address"
+    } else {
+        error = "Give us a correct age (1 - 99)"
+    }
+
+    showLogInError(error)   
+}
+
+function showLogInError(error) {
+    const p = document.querySelector("#loginerror")
+    p.innerText = error
+    p.style.display = "block"
+}
+
+function validData() {
+    const email = document.querySelector('input[type="email"]').value;
+    const age = document.querySelector('input[type="number"]').value;
+
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const isEmailValid = emailPattern.test(email);
+
+    const isAgeValid = age >= 1 && age <= 99;
+
+    const isDifficultySelected = difficulty != null
+
+    return [isEmailValid, isAgeValid, isDifficultySelected];
+}
+
+
+function startGame(actCards) {
+    document.querySelector("#login").style.display = "none"
+    document.querySelector("#game").style.display = "block"
+}
+
+function generateCards() {
+    let actCards
+    switch (difficulty) {
+        case "easy":
+            actCards = CARDS.slice(0, 6)
+            break;
+        case "medium":
+            actCards = CARDS.slice(0, 9)
+            break;
+        case "hard":
+            actCards = CARDS.slice(0, 12)
+            break;
+        default:
+            break;
+    }
+
+    return actCards
+}
 
 const dificultyDivs = document.querySelector("#difficulties")
 dificultyDivs.addEventListener("click", handleDificultySelection)
 
 function handleDificultySelection(e) {
     const button = e.target
+    
     if(button.matches(".difficultyButton")) {
         if (difficulty != null) {
+            
             document.querySelector("#" + difficulty).classList.remove("selected")
         }
 
@@ -20,37 +100,13 @@ const input = document.querySelector('input[type="number"]');
 input.addEventListener("change" , ()=> {
         const value = parseInt(input.value);
         if (isNaN(value)) {
-            input.value = ""; // Ha nem szám, töröljük
+            input.value = "";
         } else if (value < 1) {
-            input.value = 1; // Minimum 1
+            input.value = 1;
         } else if (value > 99) {
-            input.value = 99; // Maximum 99
+            input.value = 99;
         }
-    
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 const div = document.querySelector("#cards");
 div.addEventListener("click", handleFlip);
@@ -67,7 +123,6 @@ function handleFlip(e) {
             front.classList.remove("flipped");
         }, 1000);
     }
-    
 }
 
 
